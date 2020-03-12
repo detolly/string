8ae0f54b212a9d77d4a3a2118873c0b9d4067499
Thomas Hammer <h584962@stud.hvl.no>
2020-03-13 00:39:14 +0100

    Some changes

--- main.cpp
+++ main.cpp
@@ -9,10 +9,11 @@ using namespace detolly::string;
 int Test() {
 	{
 		string a;
-		string b = "b";
+		string b = "bbbb";
 		auto t1 = std::chrono::high_resolution_clock::now();
 		for (int i = 0; i < 100000; i++) {
-			a = a + b;
+			//still spend a lot of time on this particular problem
+			a = a + "bbbb";
 		}
 		auto t2 = std::chrono::high_resolution_clock::now();
 		auto time = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
@@ -20,10 +21,10 @@ int Test() {
 	}
 	{
 		std::string a;
-		std::string b = "b";
+		std::string b = "bbbb";
 		auto t1 = std::chrono::high_resolution_clock::now();
 		for (int i = 0; i < 100000; i++) {
-			a = a + b;
+			a = a + "bbbb";
 		}
 		auto t2 = std::chrono::high_resolution_clock::now();
 		auto time = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
